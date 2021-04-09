6ea5302f919e61fdf4264a3a2fb34d4e2b8953fb
tomokiqi <tomokiqi@gmail.com>
2021-04-09 22:52:53 +0900

    add comment to learn

--- lib/hello_world.ex
+++ lib/hello_world.ex
@@ -8,5 +8,20 @@ defmodule HelloWorld do
   end
 end
 
+# 処理をループする
 # iex hello_world.ex
-# pid = spawn(HelloWorld, :greet, ["Taro"])
+# pid = spawn(HelloWorld, :greet, ["Tom"])
+# send pid, "Tom"
+
+
+# iexごと終了する
+# pid = spawn_link(HelloWorld, :greet, ["Tom"])
+# send pid, "Tom"
+# Process.exit(pid, "Exit!!")
+
+# iexは終了しないようにシステムプロセス化する
+# pid = spawn_link(HelloWorld, :greet, ["Tom"])
+# send pid, "Tom"
+# Process.flag(:trap_exit, true)
+# Process.exit(pid, "Exit!!")
+# flush()
